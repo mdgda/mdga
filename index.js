45e0dc1b64d855310c865b6e691e4bd2f90a0b50
mdga <4utistickid@gmail.com>
2026-04-28 17:32:20 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+
+//window.alert("se hizo un cambio");
+//comentario
+
+/*
+comentario
+largo
+ */
+
+let nombre = "mdga";
+document.getElementById("tituloaside").textContent = nombre;
+
+//variables
+let x;
+x = 100;
+console.log(`${x} eso tilin`);
+
+
+/*document.addEventListener('mousemove', (e) => {
+
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+
+    const mouseXPercent = (mouseX / viewportWidth) * 100;
+    const mouseYPercent = (mouseY / viewportHeight) * 100;
+
+
+    document.body.style.setProperty('--mouse-x', `${mouseXPercent}%`);
+    document.body.style.setProperty('--mouse-y', `${mouseYPercent}%`);
+
+    document.body.style.setProperty('--mouse-x', `${mouseXPercent}%`);
+    document.body.style.setProperty('--mouse-y', `${mouseYPercent}%`);
+});
+
+ */
